f0897d6e1da479fab05d65ce939a82e6fb88406e
Romaana <b2521139@ben.edu>
2022-09-25 03:02:32 -0500

    reg exp site

--- client/regexp.js
+++ client/regexp.js
@@ -1,8 +1,15 @@
 // Compare two input fields in js
 function compare_input(){
-    var f_input=document.getElementById('first_input').value;
-    var s_input=document.getElementById('second_input').value;
-    if(f_input===s_input){
-       alert('same as first');
-    }
-  }
+   var f_input=document.getElementById('first_input').value;
+   var s_input=document.getElementById('second_input').value;
+   if(f_input===s_input){
+     
+
+     
+      alert('true');
+   }
+   else if(f_input != s_input){
+     alert('false');
+
+   }
+ }
