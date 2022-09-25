b6ca1f44e039f1aa4fb391f7bacb0d3dce0d2b2d
Romaana <b2521139@ben.edu>
2022-09-25 02:45:19 -0500

    reg exp site

---
+++ client/regexp.js
@@ -0,0 +1,8 @@
+// Compare two input fields in js
+function compare_input(){
+    var f_input=document.getElementById('first_input').value;
+    var s_input=document.getElementById('second_input').value;
+    if(f_input===s_input){
+       alert('same as first');
+    }
+  }
